3a6c9112d84b623a824845fdc46afc77aee7d65b
mi18145 <mi18145@alas.matf.bg.ac.rs>
2023-08-25 11:23:25 +0200

    Change: a couple UnitTests, added a few more and added comments

--- TimesweeperUnitTests/tst_timesweeper.cpp
+++ TimesweeperUnitTests/tst_timesweeper.cpp
@@ -5,7 +5,7 @@
 #include <Headers/Game.h>
 #include <Headers/Level.h>
 #include <Headers/PlayerCharacter.h>
-#include <Headers/DialogueHandler.h>
+#include <Headers/Menu.h>
 
 
 class timesweeper : public QObject
@@ -15,12 +15,14 @@ class timesweeper : public QObject
 public:
     timesweeper();
     ~timesweeper();
-    Game* game;
+    Game* testGame;
+    PlayerCharacter* testPlayer;
 
 private slots:
+    void init();
     void cleanup();
 
-    // Game creation
+    // Game
     // LevelID should be 2 as changeLevel should be called when instantiating Game
     void testIsLevelID2AfterGameCreation();
     // PlayerCharacter should be created with the Game instance
@@ -35,16 +37,12 @@ private slots:
     void testGameOverLabelCreatedAndHiddenAfterGameCreation();
     // A PauseLabel should be created with the Game instance
     void testPauseLabelCreatedAndHiddenAfterGameCreation();
-
-    // changeLevel
     // levelID should increase when changeLevel is called
     void testIsLevelIDChangesAfterChangeLevel();
     // QGraphicsScene should change when changeLevel is called
     void testIsLevelChangedAfterChangeLevel();
     // PlayerCharacter should persist through changeLevel calls
     void testPlayerPersistsThroughChangeLevel();
-
-    // gameOver
     // QTimer should stop when gameOver is called
     void testMainTimerStopedWhenGameOver();
     // QMediaPlayer should be pased when gameOver is called
@@ -54,6 +52,21 @@ private slots:
     // GameOverLabel should be shown when gameOver is called
     void testGameOverLabelShownWhenGameOver();
 
+    // PlayerCharacter
+    // gunArm should be initialized with the PlayerCharacter
+    void testIsGunArmInitializedAfterPlayerCreation();
+    // shoulderPosition should be set with the creation of the PlayerCharacter
+    void testIsShoulderPositionSetAfterPlayerCreation();
+    // increaseHealth should not change hp when at max hp
+    void testIncreaseHealthWhileMaxHealth();
+    // increaseHealth should increase the hp by 1 when not at max hp
+    void testIncreaseHealthWhileNotMaxHealth();
+    // decreaseHealth should lower the hp by 1 when not at 0 hp
+    void testDecreaseHealthWhileNotAt0Health();
+    // decreaseHealth should emit playerIsDead signal when at 0 hp
+    void testDecreaseHealthWhileAt0Health();
+    // shootProjectile should create a new entity and add it to the currentLevel items
+    void testShootProjectileCreatesNewProjectile();
 };
 
 timesweeper::timesweeper()
@@ -66,117 +79,197 @@ timesweeper::~timesweeper()
 
 }
 
+void timesweeper::init()
+{
+    testGame = nullptr;
+    testPlayer = nullptr;
+}
+
 void timesweeper::cleanup(){
-    delete game;
+    delete testGame;
+    delete testPlayer;
 }
 
 void timesweeper::testIsLevelID2AfterGameCreation()
 {
-    game = new Game();
+    testGame = new Game();
     int expectedValue = 2;
 
-    QCOMPARE(game->getLevelID(),expectedValue);
+    QCOMPARE(testGame->getLevelID(),expectedValue);
 }
 
 void timesweeper::testIsPlayerInitialisedAfterGameCreation()
 {
-    game = new Game();
-    QVERIFY(game->player != nullptr);
+    testGame = new Game();
+    QVERIFY(testGame->player != nullptr);
 }
 
 void timesweeper::testIsSoundOnAfterGameCreation()
 {
-    game = new Game();
+    testGame = new Game();
     bool expectedValue = true;
 
-    QCOMPARE(game->getSoundOn(),expectedValue);
+    QCOMPARE(testGame->getSoundOn(),expectedValue);
 }
 
 void timesweeper::testMediaPlayerSetAfterGameCreation()
 {
-    game = new Game();
-    QVERIFY(game->music != nullptr);
+    testGame = new Game();
+    QVERIFY(testGame->music != nullptr);
 }
 
 void timesweeper::testMainTimerSetAfterGameCreation()
 {
-    game = new Game();
-    QVERIFY(game->mainTimer != nullptr);
+    testGame = new Game();
+    QVERIFY(testGame->mainTimer != nullptr);
 }
 
 void timesweeper::testGameOverLabelCreatedAndHiddenAfterGameCreation()
 {
-    game = new Game();
-    QVERIFY(game->getGameOverLabel() != nullptr);
-    QVERIFY(game->getGameOverLabel()->isHidden());
+    testGame = new Game();
+    QVERIFY(testGame->getGameOverLabel() != nullptr);
+    QVERIFY(testGame->getGameOverLabel()->isHidden());
 }
 
 void timesweeper::testPauseLabelCreatedAndHiddenAfterGameCreation()
 {
-    game = new Game();
-    QVERIFY(game->getPauseLabel() != nullptr);
-    QVERIFY(game->getPauseLabel()->isHidden());
+    testGame = new Game();
+    QVERIFY(testGame->getPauseLabel() != nullptr);
+    QVERIFY(testGame->getPauseLabel()->isHidden());
 }
 
 void timesweeper::testIsLevelIDChangesAfterChangeLevel()
 {
-    game = new Game();
-    int levelIDBeforeChangeLevel = game->getLevelID();
-    game->changeLevel();
+    testGame = new Game();
+    int levelIDBeforeChangeLevel = testGame->getLevelID();
+    testGame->changeLevel();
 
-    QCOMPARE(game->getLevelID(), levelIDBeforeChangeLevel + 1);
+    QCOMPARE(testGame->getLevelID(), levelIDBeforeChangeLevel + 1);
 }
 
 void timesweeper::testIsLevelChangedAfterChangeLevel()
 {
-    game = new Game();
-    QGraphicsScene* levelBeforeChangeLevel = game->currentLevel;
-    game->changeLevel();
-    QGraphicsScene* levelAfterChangeLevel = game->currentLevel;
+    testGame = new Game();
+    QGraphicsScene* levelBeforeChangeLevel = testGame->currentLevel;
+    testGame->changeLevel();
+    QGraphicsScene* levelAfterChangeLevel = testGame->currentLevel;
 
     QVERIFY(levelAfterChangeLevel != levelBeforeChangeLevel);
 }
 
 void timesweeper::testPlayerPersistsThroughChangeLevel()
 {
-    game = new Game();
-    PlayerCharacter* playerBeforeChangeLevel = game->player;
-    game->changeLevel();
-    PlayerCharacter* playerAfterChangeLevel = game->player;
+    testGame = new Game();
+    PlayerCharacter* playerBeforeChangeLevel = testGame->player;
+    testGame->changeLevel();
+    PlayerCharacter* playerAfterChangeLevel = testGame->player;
 
     QCOMPARE(playerAfterChangeLevel,playerBeforeChangeLevel);
 }
 
 void timesweeper::testMainTimerStopedWhenGameOver()
 {
-    game = new Game();
-    game->gameOver();
+    testGame = new Game();
+    testGame->gameOver();
 
-    QVERIFY(!game->mainTimer->isActive());
+    QVERIFY(!testGame->mainTimer->isActive());
 }
 
 void timesweeper::testMusicPausedWhenGameOver()
 {
-    game = new Game();
-    game->gameOver();
+    testGame = new Game();
+    testGame->gameOver();
 
-    QVERIFY(game->music->state() == QMediaPlayer::PausedState);
+    QVERIFY(testGame->music->state() == QMediaPlayer::PausedState);
 }
 
 void timesweeper::testIsGameOverTrueWhenGameOver()
 {
-    game = new Game();
-    game->gameOver();
+    testGame = new Game();
+    testGame->gameOver();
 
-    QVERIFY(game->getIsGameOver());
+    QVERIFY(testGame->getIsGameOver());
 }
 
 void timesweeper::testGameOverLabelShownWhenGameOver()
 {
-    game = new Game();
-    game->gameOver();
+    testGame = new Game();
+    testGame->gameOver();
 
-    QVERIFY(!game->getGameOverLabel()->isHidden());
+    QVERIFY(!testGame->getGameOverLabel()->isHidden());
+}
+
+void timesweeper::testIsGunArmInitializedAfterPlayerCreation()
+{
+    testPlayer = new PlayerCharacter();
+
+    QVERIFY(testPlayer->getGunArm() != nullptr);
+}
+
+void timesweeper::testIsShoulderPositionSetAfterPlayerCreation()
+{
+    testPlayer = new PlayerCharacter();
+
+    QCOMPARE(testPlayer->shoulderPosition, testPlayer->pos()+ QPointF(35, 60));
+}
+
+void timesweeper::testIncreaseHealthWhileMaxHealth()
+{
+    testPlayer = new PlayerCharacter();
+    int healthBefore = testPlayer->getHealth();
+    testPlayer->increaseHealth();
+    int healthAfter = testPlayer->getHealth();
+
+    QCOMPARE(healthAfter, healthBefore);
+}
+
+void timesweeper::testIncreaseHealthWhileNotMaxHealth()
+{
+    testPlayer = new PlayerCharacter();
+    testPlayer->decreaseHealth();
+    int healthBefore = testPlayer->getHealth();
+    testPlayer->increaseHealth();
+    int healthAfter = testPlayer->getHealth();
+
+    QCOMPARE(healthAfter, healthBefore + 1);
+}
+
+void timesweeper::testDecreaseHealthWhileNotAt0Health()
+{
+    testPlayer = new PlayerCharacter();
+    int healthBefore = testPlayer->getHealth();
+    testPlayer->decreaseHealth();
+    int healthAfter = testPlayer->getHealth();
+
+    QCOMPARE(healthAfter, healthBefore - 1);
+}
+
+void timesweeper::testDecreaseHealthWhileAt0Health()
+{
+    testPlayer = new PlayerCharacter();
+    QSignalSpy signalSpy(testPlayer, SIGNAL(playerIsDead()));
+
+    // lowering the players health to 0
+    while(testPlayer->getHealth() != 0)
+        testPlayer->decreaseHealth();
+
+    // one last call should trigger the signal emission
+    testPlayer->decreaseHealth();
+
+    QCOMPARE(signalSpy.count(),1);
+}
+
+void timesweeper::testShootProjectileCreatesNewProjectile()
+{
+    Menu m;
+    extern Game* game;
+    testGame = game;
+    testPlayer = new PlayerCharacter();
+    int numberOfObjectsBeforeShoot = testGame->currentLevel->items().count();
+    testPlayer->shootProjectile();
+    int numberOfObjectsAfterShoot = testGame->currentLevel->items().count();
+
+    QCOMPARE(numberOfObjectsAfterShoot, numberOfObjectsBeforeShoot + 1);
 }
 
 QTEST_MAIN(timesweeper)
